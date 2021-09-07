cdb24da565d9ffe5d52411d4e77ba26430ef0ee2
Tobias <tobiaslamanna@gmail.com>
2021-09-07 20:55:47 -0300

    creacion Main y TrivialTest

---
+++ src/main/java/pokedex/lite/Main.java
@@ -0,0 +1,10 @@
+package pokedex.lite;
+
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("pokedex-lite");
+    }
+}
---
+++ src/test/test/pokedex/lite/TrivialTest.java
@@ -0,0 +1,14 @@
+package pokedex.lite;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TrivialTest {
+
+    @Test
+    public void TestTrivialDevuelveTrue(){
+        assertTrue(true);
+    }
+
+}
